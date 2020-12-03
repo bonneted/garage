4985533065ee07554b3a3ab170d6a7faf8d3629d
Bonneted <bonneteymard.damien@gmail.com>
2020-12-03 21:16:14 +0100

    update

--- bdd_form/static/bdd_form/js/main.js
+++ bdd_form/static/bdd_form/js/main.js
@@ -62,5 +62,23 @@ const loading_status = JSON.parse(document.getElementById('loading_status').text
 console.log(loading_status)
 
 
+// tab gestion
 
+function openPage(pageName,elmnt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = "white";
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = '#f5f5f5';
+  elmnt.style.color = 'black';
+}
 
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
